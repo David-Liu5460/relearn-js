b574c4c0895a389eedf3c6454f90c707464628a9
zhengmeng <zhengmeng.lm@alibaba-inc.com>
2022-05-06 09:51:22 +0800

    fix: +1;

---
+++ 第12章 迭代器与生成器/02.iterator.js
@@ -0,0 +1,37 @@
+class Range {
+  constructor (from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  
+  // 让range对象像数值的集合一样
+  has (x) {
+    return typeof x === 'number' && this.from <= x && x <= this.to;
+  }
+
+  toString () {
+    return `{x | ${this.from} <= x <= ${this.to}}`;
+  }
+
+  [Symbol.iterator]() {
+    let next = Math.ceil(this.from);
+    let last = this.to;
+    return {
+      next () {
+        return (next < last) 
+            ? { value : next++ }
+            : { done: true };
+      },
+
+      [Symbol.iterator]() { return this; };
+    };
+  };
+}
+
+var myIterable = {};
+myIterable[Symbol.iterator] = function* () {
+  yield 1;
+  yield 2;
+  yield 3;
+};
+console.log([...myIterable]);
